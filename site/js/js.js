09c06ba9da32ab720ac19cbf15b049a0f0d53aa6
Jovarod <jovanyrodriguez@gmail.com>
2023-02-20 09:07:27 -0500

    Mi primera pagina.

---
+++ site/js/js.js
@@ -0,0 +1,6 @@
+// Cargar los íconos de Font Awesome
+const head = document.querySelector('head');
+const link = document.createElement('link');
+link.rel = 'stylesheet';
+link.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css';
+head.appendChild(link);
